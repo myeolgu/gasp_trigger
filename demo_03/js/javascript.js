73bcf21ad20ad110e84d915e5f827b3e1042920a
leejuyeop <juyeop198@etribe.co.kr>
2023-02-24 17:18:51 +0900

    일단 저장

--- demo_03/js/javascript.js
+++ demo_03/js/javascript.js
@@ -1,18 +1,32 @@
-// set height of the svg path as constant
-const svg = document.getElementById("svgPath");
-const length = svg.getTotalLength();
+gsap.registerPlugin(ScrollTrigger);
 
-// start positioning of svg drawing
-svg.style.strokeDasharray = length;
+let container = document.querySelector(".slides"),
+    slides = gsap.utils.toArray(".slide"),
+    getRatio = (el) => window.innerHeight / (window.innerHeight + el.offsetHeight);
 
-// hide svg before scrolling starts
-svg.style.strokeDashoffset = length;
+slides.forEach((slide, i) => {
+  let bg = slide.querySelector(".background"),
+      content = slide.querySelector(".content"),
+      tl = gsap.timeline({
+            scrollTrigger: {
+              trigger: slide,
+              start: () => i ? "top bottom" : "top top",
+              end: "bottom top",
+              scrub: true,
+              invalidateOnRefresh: true
+            }
+          });
 
-window.addEventListener("scroll", function () {
-  const scrollpercent = (document.body.scrollTop + document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
-
-  const draw = length * scrollpercent;
-
-  // Reverse the drawing when scroll upwards
-  svg.style.strokeDashoffset = length - draw;
+  tl.fromTo(bg, {
+      y: () => i ? -window.innerHeight * getRatio(slide) : 0
+    }, {
+      y: () => window.innerHeight * (1 - getRatio(slide)),
+      ease: "none"
+    });
+  tl.fromTo(content, {
+      y: () => i ? window.innerHeight * -getRatio(slide) * 2 : 0
+    }, {
+      y: () => window.innerHeight * getRatio(slide) * 2,
+      ease: "none"
+    }, 0);
 });
